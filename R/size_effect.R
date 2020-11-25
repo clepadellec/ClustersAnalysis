628e2f4ec613d54266f43afc7ca45aef8c7576e5
clepadellec <clem.lepadellec@gmail.com>
2020-11-25 17:17:04 +0100

    tests about size_effect function

---
+++ R/size_effect.R
@@ -0,0 +1,16 @@
+
+#' Title
+#'
+#' @param df the dataframe
+#' @param var_expli
+#' @param target_grp
+#'
+#' @return a description (distribution) of size effect between a group choose by the user and an other variable(for each modality)
+#' @export
+#'
+#' @examples
+desc_size_effect <- function(df,var_expli,target_grp){
+  #à compléter
+  #http://eric.univ-lyon2.fr/~ricco/cours/slides/effect_size.pdf | slide 18
+
+}
