1b675d9ae0cddc6a434e65a8cbcb1c1acf02ecd4
clepadellec <clem.lepadellec@gmail.com>
2020-11-29 15:49:58 +0100

    add shapiro test for small pop, formatting res in a matrix

--- R/generalisation_quanti.R
+++ R/generalisation_quanti.R
@@ -1,61 +1,22 @@
-### Groupes = Régime_simp ###
 
-# detection_quanti <- function(data){
-#   # Repère var quanti
-#   type_var <- data %>% head %>% collect %>% lapply(class) %>% unlist
-#   # Repère index var quanti
-#   var_quanti <- c(which(type_var == "numeric" | type_var == "integer"))
-#   # Supprime var quanti
-#   data <- data[,var_quanti]
-#
-#   return(data)
-# }
-#
-#
-# shapiro_test <- function(data){
-#   res <- matrix(data = NA, nrow = (ncol(data)-1), ncol = 3)
-#   for (j in 1:(ncol(data)-1)){
-#     res[j,1] <- colnames(data[j])
-#     res[j,2] <- shapiro.test(data[,j])$p.value
-#     if (round(as.numeric(res[j,2]),5)<0.05){
-#       res[j,3]<-"significatif"
-#     }else{
-#       res[j,3]<-"non significatif"
-#     }
-#   }
-#   res <- as.data.frame(res)
-#   colnames(res)<-c("var.explicative","p.value.shapiro.test","interpretation")
-#   return(res)
-# }
-#
-# data=read.csv2("Données-R-1.csv", sep=";", row.names = 1)
-#
-# #recupérer toutes var quanti
-# data_quanti = detection_quanti(data)
-# # data_quanti_tot = merge(data_quanti, data_0$heureux_quali)
-# #
-# # #spliter en fonction des groupes
-# # X <- split(data_quanti_tot[1:29,], data_quanti_tot[1:29,]$y)
-# X <- split(data_quanti, data$Régime_simp)
-# #
-# # #regarder taille
-# for (i in 1:length(X)){
-#   if (nrow(X[[i]]) < 30){
-#     res = shapiro_test(X[[i]])
-#    #garde seulement les variables significatives
-#     ind = which(res$interpretation == "significatif")
-#     var.exp = c(res$var.explicative[ind])
-#   }
-# }
-#
-# #cherche noms colonnes
-# ind = which(colnames(data) == colnames(data_quanti))
-# data[ind]
-#
-# #anova
-# a<-aov(data[ind] ~ data$Regime_poli)
-# b <- summary(a)
-# names(b[2])
+
+#' Title
+#'
+#' @param pop_a
+#' @param pop_b
+#'
+#' @return
+#' @export
+#'
+#' @examples
+shapiro_test <- function(pop_a,pop_b){
+  pvalue_pop_a <- shapiro.test(pop_a)$p.value
+  pvalue_pop_b <- shapiro.test(pop_b)$p.value
+  if((round(as.numeric(pvalue_pop_a),5)>0.05)|length(pop_a)>30){bool_a=TRUE}else{bool_a=FALSE}
+  if((round(as.numeric(pvalue_pop_b),5)>0.05)|length(pop_b)>30){bool_b=TRUE}else{bool_b=FALSE}
+  if(bool_a==TRUE & bool_b==TRUE){hyp_gauss=TRUE}else{hyp_gauss=FALSE}
+  return(hyp_gauss)
+}
 
 
 #' Title
@@ -67,7 +28,7 @@
 #' @export
 #'
 #' @examples
-aov_all <- function(df,var_groupe){
+ttest_all <- function(df,var_groupe){
     type_var <- df %>% head %>% collect %>% lapply(class) %>% unlist
     # Repère index var quanti
     var_quanti <- c(which(type_var == "numeric" | type_var == "integer"))
@@ -77,18 +38,36 @@ aov_all <- function(df,var_groupe){
     data <- cbind(data,var_groupe)
     lst_grp<-unique(var_groupe)
     #print(data$var_groupe)
+    res <- matrix(data = NA, nrow = length(lst_grp), ncol = length(var_quanti))
+    lig=1
     for (i in lst_grp){
+      col=1
       new_data=data
       new_data$var_groupe[new_data$var_groupe != i] <- "others"
-      for (j in 1:length(var_quanti)){
-        print("-------------------------------------------")
-        print(paste("i : ",i))
-        print(paste("j : ",j))
-        print(t.test(new_data[,j]~new_data$var_groupe)$p.value)
-      }
+      pop_a=new_data[new_data$var_groupe==i,]
+      pop_b=new_data[new_data$var_groupe!=i,]
+      gauss=TRUE
+      if(nrow(pop_a)<30|nrow(pop_b)<30){gauss=FALSE}
 
+      for (j in 1:length(var_quanti)){
+        hyp_gauss=TRUE
+        if (gauss==FALSE){
+          hyp_gauss <- shapiro_test(pop_a[,j],pop_b[,j])
+        }
+        if (hyp_gauss==TRUE){
+          pv=t.test(new_data[,j]~new_data$var_groupe)$p.value
+          if(round(as.numeric(pv),5)<0.05){res[lig,col]="significatif"}else{res[lig,col]="non significatif"}
+
+        }else{
+          res[lig,col]=NA
+        }
+        col=col+1
+      }
+      lig=lig+1
     }
-    #return(data)
+    rownames(res)<- lst_grp
+    colnames(res)<- colnames(data[var_quanti])
+    return(res)
 }
 
 #ttest_all(data_0,'Region')
