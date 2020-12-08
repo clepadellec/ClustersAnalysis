484c1590a0b7d56bc33360c7bf925dbc8f688460
clepadellec <clem.lepadellec@gmail.com>
2020-12-08 11:18:08 +0100

    add comment on univariate class

--- R/Univariate_Class.R
+++ R/Univariate_Class.R
@@ -17,11 +17,17 @@ Univariate_object <- function(df,ind_group_class){
 
   #df_without_grp <- df[,-ind_group_class]
   instance <- list()
+
+  #test si quanti ou quali
   ind.qual <- sapply(df, function(x) is.factor(x)| is.character(x))
   ind.quan <- sapply(df, function(x) is.numeric(x)| is.integer(x))
+
+  #on recupere les indices associés
   instance$ind.qual <- ind.qual
   instance$ind.quan <- ind.quan
+  #on supprime les lignes qui contiennent des NA
   df <- na.omit(df)
+  #on passe la variable de groupe en character
   df[,ind_group_class] <- as.character(df[,ind_group_class])
   instance$df <- df
   instance$group <- ind_group_class
@@ -29,6 +35,7 @@ Univariate_object <- function(df,ind_group_class){
   instance$lst_quali <- colnames(df[ ,ind.qual])
   instance$lst_quanti <- colnames(df[ ,ind.quan])
 
+  #on regarde si il y a plusieurs vars explicatives ou non
   if ((length(ind.qual)+length(ind.quan))>1){
     instance$multiple_var=TRUE
   }else{
@@ -51,6 +58,7 @@ Univariate_object <- function(df,ind_group_class){
 #'
 u_desc_size_effect <- function(object,ind_var_exp){
 
+  #test si la variable est bien quali
   if (is.character(object$df[[ind_var_exp]])==FALSE & is.factor(object$df[[ind_var_exp]])==FALSE){
     return("Votre variable explicative n'est pas qualitative ! ")
     stop()
@@ -65,6 +73,7 @@ u_desc_size_effect <- function(object,ind_var_exp){
   #pour chaque paire de modalités on calcule le vtest
   for (i in 1:nrow(tab_base)){
     for (j in 1:ncol(tab_base)){
+
       prop_l_g <- tab[i,j]/tab[i,ncol(tab_base)+1]
       prop_l <- tab[nrow(tab_base)+1,j]/tab[nrow(tab_base)+1,ncol(tab_base)+1]
       r=sqrt((tab[nrow(tab_base)+1,ncol(tab_base)+1]-tab[i,ncol(tab_base)+1])/(tab[nrow(tab_base)+1,ncol(tab_base)+1]-1))
@@ -85,6 +94,7 @@ u_desc_size_effect <- function(object,ind_var_exp){
 #' @examples u_chisq_test_all(Univariate_object(esoph,1))
 u_chisq_test_all <- function(object){
   options(warn=-1)
+  #Test s'il existe des vars quali
   if (length(object$ind.qual[object$ind.qual==TRUE])==1){
     return("Aucunes variables explicatives de votre dataframe n'est qualitative !")
     stop()
@@ -165,6 +175,7 @@ u_desc_profils <- function(object,ind_var_exp,interact=TRUE){
   print(lprop(contingence, digits=1))
   print("Profils colonnes : ")
   print(cprop(contingence, digits=2))
+  #on effectue un barplot pour visualiser la distribution des classes/Modalités
   p <-ggplot(tab, aes(fill = explanatory, y = Freq, x = cluster)) + geom_bar(position ="stack", stat = "identity")
   if (interact==TRUE){return(ggplotly(p))}else{return(p)}
 
@@ -183,16 +194,22 @@ u_desc_profils <- function(object,ind_var_exp,interact=TRUE){
 #'
 #' @examples
 u_Mosaic_plot <- function(explanatory, cluster){
+
+  #calcul des longueurs de vecteur
   levVar1 <- length(levels(explanatory))
   levVar2 <- length(levels(cluster))
 
+  #creation contingence mais en proportion
   jointTable <- prop.table(table(explanatory, cluster))
+  #transformation en df
   data <- as.data.frame(jointTable)
+  #calcul des valeurs à afficher
   data$marginVar1 <- prop.table(table(explanatory))
   data$Clusters <- data$Freq / data$marginVar1
   data$Explanatory <- c(0, cumsum(data$marginVar1)[1:levVar1 -1]) +
     data$marginVar1 / 2
 
+  #creation d'un mosaic plot à l'aide de barplot
   g<- ggplot(data, aes(Explanatory, Clusters)) +
     geom_bar(stat = "identity", aes(width = marginVar1, fill = cluster), col = "Grey") +
     geom_text(aes(label = as.character(explanatory), x = Explanatory, y = 1.1))
@@ -216,13 +233,17 @@ u_Mosaic_plot <- function(explanatory, cluster){
 #'
 #' @examples u_plot_size_effect((Univariate_object(esoph,1)),2)
 u_plot_size_effect<- function(object,name_var_exp, interact=TRUE){
+  #on recup les données sur la var de groupe et la var explicative
   var_groupe <- object$name_group
   x=object$df[[name_var_exp]]
   y=object$df[[var_groupe]]
 
+  #on créer le df correspondant
   df=data.frame("explanatory"=x, "cluster"=y)
+  #on créer un objet ggplot qui correspond au mosaic plot
   p <- u_Mosaic_plot(df$explanatory,df$cluster)
 
+  #si l'utilisiteur veut un graph interactif alors on utilise ggplotly sinon on retourne juste l'objet ggplot
   if (interact==TRUE){return(ggplotly(p))}else{return(p)}
 
 }
@@ -237,10 +258,15 @@ u_plot_size_effect<- function(object,name_var_exp, interact=TRUE){
 #'
 #' @examples
 u_shapiro_test <- function(pop_a,pop_b){
+  #on effectue le test de shapiro sur les deux pop
   pvalue_pop_a <- shapiro.test(pop_a)$p.value
   pvalue_pop_b <- shapiro.test(pop_b)$p.value
+
+  #test si la p-value est >5% ou pop >30
   if((round(as.numeric(pvalue_pop_a),5)>0.05)|length(pop_a)>30){bool_a=TRUE}else{bool_a=FALSE}
   if((round(as.numeric(pvalue_pop_b),5)>0.05)|length(pop_b)>30){bool_b=TRUE}else{bool_b=FALSE}
+
+  #si les deux ech sont gaussiens alors true sinon false
   if(bool_a==TRUE & bool_b==TRUE){hyp_gauss=TRUE}else{hyp_gauss=FALSE}
   return(hyp_gauss)
 }
@@ -254,29 +280,43 @@ u_shapiro_test <- function(pop_a,pop_b){
 #'
 #' @examples u_ttest_all(Univariate_object(esoph,1))
 u_ttest_all <- function(object){
+
+  #pretraitement
   df <- na.omit(object$df)
   var_groupe <- object$name_group
   data= df[ ,object$ind.quan]
+  #liste des groupes
   lst_grp <-unique(df[[var_groupe]])
+  #données correpondant à la variable de groupe
   var_groupe <-df[[var_groupe]]
 
   data <- cbind(data,var_groupe)
+
+  #creation de la matrice qu'on retournera
   res <- matrix(data = NA, nrow = length(lst_grp), ncol = length(object$ind.quan[object$ind.quan==TRUE]))
   lig=1
+  #pour chaque classe
   for (i in lst_grp){
     col=1
+    #on recode les modalités qui ne sont pas notre classe cible en "others"
     new_data=data
     new_data$var_groupe[new_data$var_groupe != i] <- "others"
+
+    #on créer les deux populations à tester
     pop_a=new_data[new_data$var_groupe==i,]
     pop_b=new_data[new_data$var_groupe!=i,]
     gauss=TRUE
+
+    #on teste la taille des echantillons pour savoir si on considère la normalité
     if(nrow(pop_a)<30|nrow(pop_b)<30){gauss=FALSE}
     for (j in 1:length(object$ind.quan[object$ind.quan==TRUE])){
       hyp_gauss=TRUE
       if (gauss==FALSE){
+        #si hypothèse de normalité pas confirmée par la taille de l'ech alors on fait un test de shapiro
         hyp_gauss <- u_shapiro_test(pop_a[,j],pop_b[,j])
       }
       if (hyp_gauss==TRUE){
+        #calcul et interpretation de la pvalue (t.test)
         pv=t.test(new_data[,j]~new_data$var_groupe)$p.value
         if(round(as.numeric(pv),5)<0.05){res[lig,col]="significatif"}else{res[lig,col]="non significatif"}
 
@@ -322,9 +362,11 @@ u_test_value=function(object,i=1){
   moyenne=apply(data, MARGIN = 2,mean)
   len=tapply(data[,1],g, FUN = length)
   for ( j in 1:n_unique){
+    #calcul de la valeur test
     VT=(moyenne_group[,j]-moyenne)/sqrt(((n-len[j])/(n-1)*(variance/len[j])))
     pvalue=rep(0,length(VT))
     for (k in 1:length(VT)){
+      #calcul de la pvalue associée à la valeur test
       if (VT[k]<0){
         pvalue[k]=2*pnorm(VT[k])
       } else{
@@ -354,10 +396,16 @@ u_test_value=function(object,i=1){
 #'
 #' @examples
 u_eta2=function(x,g){
+
+  #calcul des moyennes
   moyenne=tapply(x,g, FUN = mean)
+  #calcul du nb d'indi
   individu=tapply(x,g,FUN = length)
+  #calcul de la variance inter
   var_inter=sum(individu*((moyenne-mean(x))^2))
+  #calcul de la variance totale
   var_total=sum((x-mean(x))^2)
+  #calcul du rapport de corrélation
   eta=var_inter/var_total
   return(eta)
 }
@@ -376,8 +424,11 @@ u_fisher_test_all=function(object){
   g=object$df[[var_groupe]]
   n=nrow(data)
   K=length(unique(g))
+  #calculs des rapports de corrélation associés
   Eta2=apply(data,MARGIN = 2,FUN = function(x){return(u_eta2(x,g))})
+  #calcul de la valeur test
   Test_value=(n-K)/(K-1)*(Eta2)/(1-Eta2)
+  #puis de la p-value
   p_value=1-pf(Test_value,K-1,n-K)
   df=data.frame('Eta2'=Eta2, 'Test_value'=Test_value, 'p_value'=p_value)
   return(df)
